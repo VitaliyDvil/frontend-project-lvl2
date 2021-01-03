1b324e7b2dacfdcb06781e6c3eac744f1ec0eccf
vitaliydvil <domanevskiyvsbel@yandex.ru>
2021-01-03 11:11:59 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,7 @@ const program = require('commander');
 
 program.version('0.0.1');
 program.description('Compares two configuration files and shows a difference.');
+program.arguments('<filepath1>, <filepath2>');
+program.option('-f, --format [type]', 'output format');
 
 program.parse(process.argv);
