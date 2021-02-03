bc35996428446bf8c1f6cb570fb60c6888c8336d
vitaliydvil <domanevskiyvsbel@yandex.ru>
2021-02-03 21:43:07 +0300

    step3

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/VitaliyDvil/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/VitaliyDvil/frontend-project-lvl2/actions)
+
+## Asciinema links:
+
+[gendiff](https://asciinema.org/a/MjhFtNMbvbgihgyLEoC5hCD4O) <!-- gendiff -->
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,15 @@
 #! /usr/bin/env node
 
+const gendiff = require('../src/index.js');
 const program = require('commander');
 
 program.version('0.0.1');
 program.description('Compares two configuration files and shows a difference.');
-program.arguments('<filepath1>, <filepath2>');
+program.arguments('<filepath1> <filepath2>');
 program.option('-f, --format [type]', 'output format');
+program.action((filepath1, filepath2) => {
+    const result = gendiff(filepath1, filepath2);
+    console.log(result);
+});
 
 program.parse(process.argv);
