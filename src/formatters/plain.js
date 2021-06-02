d6d601362e05faf4ba040a703bf1ff59d8ec3f2a
vitaliydvil <domanevskiyvsbel@yandex.ru>
2021-06-02 18:28:00 +0300

    fixed plain formatter /n

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -4,48 +4,53 @@ function setBrackets(element) {
   return typeof element === 'string' ? `'${element}'` : element;
 }
 
-function plain(diff, parentKeys = []) {
-  const result = _.reduce(
-    diff,
-    (acc, element) => {
+function plain(diff) {
+  const iter = (diff, parentKeys = []) => {
+    return _.reduce(
+      diff,
+      (acc, element) => {
 
-      //new
-      if (element.status === 'new' && !_.isPlainObject(element.value)) {
-        acc += `Property '${parentKeys.concat(element.key).join('.')}' was added with value: ${setBrackets(element.value)}\n`;
-      }
-      if (element.status === 'new' && _.isPlainObject(element.value)) {
-        acc += `Property '${parentKeys.concat(element.key).join('.')}' was added with value: [complex value]\n`;
-      }
-  
-      //deleted
-      if (element.status === 'deleted') {
-        acc += `Property '${parentKeys.concat(element.key).join('.')}' was removed\n`;
-      }
-  
-      //changed
-      if (element.status === 'changed' && !element.children) {
-        if (_.isPlainObject(element.value)) {
-          acc += `Property '${parentKeys.concat(element.key).join('.')}' was updated. From ${setBrackets(element.oldValue)} to [complex value]\n`;
+        //new
+        if (element.status === 'new' && !_.isPlainObject(element.value)) {
+          acc += `Property '${parentKeys.concat(element.key).join('.')}' was added with value: ${setBrackets(element.value)}\n`;
         }
-        else if (_.isPlainObject(element.oldValue)) {
-          acc += `Property '${parentKeys.concat(element.key).join('.')}' was updated. From [complex value] to ${setBrackets(element.value)}\n`;
-        } else {
-          acc += `Property '${parentKeys.concat(element.key).join('.')}' was updated. From ${setBrackets(element.oldValue)} to ${setBrackets(element.value)}\n`;
+        if (element.status === 'new' && _.isPlainObject(element.value)) {
+          acc += `Property '${parentKeys.concat(element.key).join('.')}' was added with value: [complex value]\n`;
         }
-      }
-      if (element.status === 'changed' && element.children) {
-        acc += `${plain(element.children, parentKeys.concat(element.key))}`;
-      }
   
-      //unchanged
-      if (element.status === 'unchanged' && element.children) {
-        acc += `${plain(element.children, parentKeys.concat(element.key))}`;
-      }
-      return acc;
-    },
-    ''
-  );
-  return result;
+        //deleted
+        if (element.status === 'deleted') {
+          acc += `Property '${parentKeys.concat(element.key).join('.')}' was removed\n`;
+        }
+  
+        //changed
+        if (element.status === 'changed' && !element.children) {
+          if (_.isPlainObject(element.value)) {
+            acc += `Property '${parentKeys.concat(element.key).join('.')}' was updated. From ${setBrackets(element.oldValue)} to [complex value]\n`;
+          }
+          else if (_.isPlainObject(element.oldValue)) {
+            acc += `Property '${parentKeys.concat(element.key).join('.')}' was updated. From [complex value] to ${setBrackets(element.value)}\n`;
+          } else {
+            acc += `Property '${parentKeys.concat(element.key).join('.')}' was updated. From ${setBrackets(element.oldValue)} to ${setBrackets(element.value)}\n`;
+          }
+        }
+        if (element.status === 'changed' && element.children) {
+          acc += `${iter(element.children, parentKeys.concat(element.key))}`;
+        }
+  
+        //unchanged
+        if (element.status === 'unchanged' && element.children) {
+          acc += `${iter(element.children, parentKeys.concat(element.key))}`;
+        }
+        return acc;
+      },
+      ''
+    );
+  }  
+
+  let result = iter(diff);
+
+  return result.substring(0, result.length - 1);
 }
   
 export default plain;
